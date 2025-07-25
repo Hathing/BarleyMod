f9c311b88f833e5823289ecfedf70e754a80a7d6
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-07-25 19:52:27 +0800

    feat(Zombie): poison damage implement

--- pvzdll/BoardEvents.cpp
+++ pvzdll/BoardEvents.cpp
@@ -7,7 +7,27 @@ void onBoardInit(MyBoard board)
 	board.PoisonCounter = 1;
 }
 
+void onBoardUpdateGameObject(MyBoard board)
+{
+	board.PoisonCounter++;
+	if (board.PoisonCounter == 10)
+	{
+		board.PoisonCounter = 0;
+
+		auto zombies = board.GetAllZombies<MyZombie>();
+		for (auto& zombie : zombies)
+		{
+			if (!zombie.PoisonStack)
+				continue;
+			if (zombie.Type == ZombieType::DrZomboss)
+				continue;
+			zombie.Hit(zombie.PoisonStack, PVZ::DAMAGEF_NOFLASH);
+		}
+	}
+}
+
 void InitBoardEvents()
 {
 	PVZEvent::BoardInitAfterEvent((int)onBoardInit);
+	UpdateGameObjectsEvent((int)onBoardUpdateGameObject);
 }
