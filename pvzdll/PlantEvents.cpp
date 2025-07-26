f30a15df6b942a231276d37fd483e0767db86c77
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-07-26 16:43:04 +0800

    feat(Plant): PlantUpdateAbilityEvent

--- pvzdll/PlantEvents.cpp
+++ pvzdll/PlantEvents.cpp
@@ -1,5 +1,15 @@
 #include "pch.h"
+#include "MyPlant/PlantAbility.hpp"
+#include "../pvzclass/Events/PlantUpdateAbilityEvent.hpp"
+
+bool onPlantUpdateAbility(MyPlant plant)
+{
+	if (plant.Type > 53)
+		return true;
+	return PlantAbility::GetPrototype(plant.Type)->TickAbility(plant);
+}
 
 void InitPlantEvents()
 {
+	PlantUpdateAbilityEvent((int)onPlantUpdateAbility);
 }
