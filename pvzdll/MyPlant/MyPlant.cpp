b1c3812db51fe295dbd186e54926ff461f639038
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-04 16:39:08 +0800

    feat(Plant): Add TallNut

--- pvzdll/MyPlant/MyPlant.cpp
+++ pvzdll/MyPlant/MyPlant.cpp
@@ -12,7 +12,7 @@ namespace PlantAbility
 		new BasePlant(), new BasePlant(), new BasePlant(), new BasePlant(),
 
 		new BasePlant(), new BasePlant(), new BasePlant(), new BasePlant(),
-		new BasePlant(), new BasePlant(), new BasePlant(), new BasePlant(),
+		new BasePlant(), new BasePlant(), new BasePlant(), new TallNut(),
 
 		new SeaShroom(), new BasePlant(), new BasePlant(), new BasePlant(),
 		new BasePlant(), new BasePlant(), new BasePlant(), new BasePlant(),
