88c244480a7793ef83282464724f864975bae06b
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-07 11:00:24 +0800

    feat(Projectile): Starfruit mark bullet source

--- pvzdll/ProjectileEvents.cpp
+++ pvzdll/ProjectileEvents.cpp
@@ -1,5 +1,15 @@
 #include "pch.h"
 
+bool onPlantAddProjectile(MyPlant plant, MyProjectile proj, MyZombie zombie)
+{
+	if(plant.Type == SeedType::Starfruit)
+	{
+		proj.ParentID = plant.Id;
+	}
+	return true;
+}
+
 void InitProjectileEvents()
 {
+	PlantAddProjectileEvent((int)onPlantAddProjectile);
 }
