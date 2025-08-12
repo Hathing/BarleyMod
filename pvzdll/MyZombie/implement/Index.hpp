ec3ae7e3e5e77731b5307b5d06418678c11f2552
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-12 19:51:13 +0800

    feat(Zombie): Add DiggerZombie

--- pvzdll/MyZombie/implement/Index.hpp
+++ pvzdll/MyZombie/implement/Index.hpp
@@ -2,6 +2,7 @@
 #include "../ZombieAbility.hpp"
 #include "PoleVaulter.hpp"
 #include "Zomboni.hpp"
+#include "DiggerZombie.hpp"
 #include "PogoZombie.hpp"
 #include "BungeeZombie.hpp"
 #include "CatapultZombie.hpp"
