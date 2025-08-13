fe5a25b1fb5326be5074dd326e8357fc934c52bb
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-13 12:49:15 +0800

    feat(Plant): LeftRepeater Attack rect

--- pvzdll/MyPlant/implement/LeftRepeater.hpp
+++ pvzdll/MyPlant/implement/LeftRepeater.hpp
@@ -9,5 +9,13 @@ namespace PlantAbility
 		{
 			plant.ShootOrProductInterval = 150;
 		}
+		bool OverwritePlantAttackRect(MyPlant plant, bool secondary, PVZ::Rect* rect)
+		{
+			rect->X = 0;
+			rect->Y = plant.ImageY;
+			rect->Width = 1000;
+			rect->Height = plant.Height;
+			return true;
+		}
 	};
 }
