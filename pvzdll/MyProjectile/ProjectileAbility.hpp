e29aecbfcb4e683de56e74d5d97f066427cb13fc
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-19 19:34:51 +0800

    feat(Proj): OverrideDamage

--- pvzdll/MyProjectile/ProjectileAbility.hpp
+++ pvzdll/MyProjectile/ProjectileAbility.hpp
@@ -1,10 +1,23 @@
 #pragma once
 #include "MyProjectile.hpp"
+#include "../MyZombie/MyZombie.hpp"
 
 namespace ProjectileAbility
 {
 	class BaseProjectile
 	{
+	public:
+		/// @brief 重载子弹对僵尸伤害的函数
+		/// @param proj 子弹
+		/// @param zombie 受伤的僵尸
+		/// @param damage_type 伤害类型
+		/// @param subtarget_count 溅射伤害溅射目标数
+		/// @param ori_dmg 溅射伤害数值
+		/// @return 若为负数，则不重载伤害，否则为重载的伤害数值
+		virtual int OverrideDamage(MyProjectile proj, MyZombie zombie, PVZEvent::ProjDmgType damage_type, int subtarget_count, int ori_dmg)
+		{
+			return -1;
+		}
 	};
 	typedef BaseProjectile* ProjectilePTR;
 	ProjectilePTR GetAbility(ProjectileType::ProjectileType type);
--- pvzdll/ProjectileEvents.cpp
+++ pvzdll/ProjectileEvents.cpp
@@ -1,4 +1,5 @@
 #include "pch.h"
+#include "MyProjectile/ProjectileAbility.hpp"
 
 bool onPlantAddProjectile(MyPlant plant, MyProjectile proj, MyZombie zombie)
 {
@@ -10,6 +11,10 @@ bool onPlantAddProjectile(MyPlant plant, MyProjectile proj, MyZombie zombie)
 
 int onProjDamageZombie(MyProjectile proj, MyZombie zombie, PVZEvent::ProjDmgType type, int subtarget_num, int damage)
 {
+	int damage = ProjectileAbility::GetAbility(proj.Type)->OverrideDamage(proj, zombie, type, subtarget_num, damage);
+	if (damage == 0)
+		return 0;
+
 	if (zombie.NotDying && !zombie.NotExist)
 	{
 		if (type == PVZEvent::ProjDmgType::DAMAGE_SINGULAR)
@@ -21,6 +26,7 @@ int onProjDamageZombie(MyProjectile proj, MyZombie zombie, PVZEvent::ProjDmgType
 				zombie.LastDamageSourceID = proj.ParentID;
 		}
 	}
+
 	return -1;
 }
 
