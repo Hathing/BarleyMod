ac9a37e65f2c6cf85cedab103ac63a91fbe7b172
Lenovo <Lenovo@LAPTOP-UEK0SPL8>
2025-08-25 18:22:44 +0800

    fix: 删除测试用代码

--- pvzdll/MyPlant/implement/Garlic.hpp
+++ pvzdll/MyPlant/implement/Garlic.hpp
@@ -1,6 +1,5 @@
 #pragma once
 #include "../PlantAbility.hpp"
-#include "../../MyBoard.hpp"
 
 namespace PlantAbility
 {
@@ -22,16 +21,6 @@ namespace PlantAbility
 					plant.Heal(1);
 				}
 			}
-
-			const int row = plant.Row;
-			if (MyBoard::GoldMagnetFactors[row] > 2.0f)plant.Remove();
-			if (MyBoard::IceShroomCounts[row])plant.Smash();
-			auto zombies = plant.GetBoard().GetAllZombies<MyZombie>();
-			for (auto& zombie : zombies)
-			{
-				if (zombie.Row==row && !zombie.Blowaway) zombie.FlyAway(-2.0);
-			}
-
 			return true;
 		}
 		bool onAnimate(MyPlant plant)
