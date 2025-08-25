528fced4a2fc157b3df4d8a2ab118366f7f60827
Lenovo <Lenovo@LAPTOP-UEK0SPL8>
2025-08-25 14:22:12 +0800

    feat(Board): 增加每行植物是否存在的更新

--- pvzdll/BoardEvents.cpp
+++ pvzdll/BoardEvents.cpp
@@ -1,11 +1,18 @@
 #include "pch.h"
 
+void InitPlantExistCount(MyBoard& board)
+{
+	std::fill(MyBoard::GoldMagnetFactors.begin(), MyBoard::GoldMagnetFactors.end(), 1.0f);
+	std::fill(MyBoard::IceShroomCounts.begin(), MyBoard::IceShroomCounts.end(), false);
+}
+
 void onBoardInit(MyBoard board)
 {
 	board.PoisonCounter = 1;
+	InitPlantExistCount(board);
 }
 
-void onBoardUpdateGameObject(MyBoard board)
+inline void UpdatePoisonApply(MyBoard& board)
 {
 	board.PoisonCounter++;
 	if (board.PoisonCounter == 10)
@@ -24,6 +31,25 @@ void onBoardUpdateGameObject(MyBoard board)
 			zombie.Hit(zombie.PoisonStack, PVZ::DAMAGEF_NOFLASH);
 		}
 	}
+}
+
+void UpdatePlantExistCount(MyBoard& board)
+{
+	InitPlantExistCount(board);
+	auto plants = board.GetAllPlants<MyPlant>();
+	for (auto& plant : plants)
+	{
+		const int row = plant.Row, col = plant.Column;
+		const auto type = plant.Type;
+		if (type == SeedType::GoldMagnet) MyBoard::GoldMagnetFactors[row] *= 1.5f;
+		if (type == SeedType::Iceshroom) MyBoard::IceShroomCounts[row] = true;
+	}
+}
+
+void onBoardUpdateGameObject(MyBoard board)
+{
+	UpdatePoisonApply(board);
+	UpdatePlantExistCount(board);
 }
 
 void InitBoardEvents()
--- pvzdll/MyBoard.hpp
+++ pvzdll/MyBoard.hpp
@@ -1,12 +1,18 @@
 #pragma once
 #include "framework.h"
 
+constexpr int MAX_BOARD_ROWS = 6;
+
 class MyBoard : public PVZ::Board
 {
 public:
 	MyBoard(int address) : PVZ::Board(address) {};
 	MyBoard(const PVZ::Board& board) : PVZ::Board(board.GetBaseAddress()) {};
 
+	/// @brief 每行吸金磁的倍数
+	inline static std::array<float, MAX_BOARD_ROWS> GoldMagnetFactors{};
+	/// @brief 每行寒冰菇是否存在
+	inline static std::array<bool, MAX_BOARD_ROWS> IceShroomCounts{};
 	/// @brief 僵尸毒结算倒计时
 	T_PROPERTY(byte, PoisonCounter, __get_PoC, __set_PoC, 0x165);
 
