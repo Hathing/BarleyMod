140409a91420cc66576789f0e2b0395bc2c99ada
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-25 22:51:38 +0800

    feat(Plant): Mangosteen upgrade

--- pvzdll/MyPlant/implement/Mangosteen.hpp
+++ pvzdll/MyPlant/implement/Mangosteen.hpp
@@ -1,13 +1,20 @@
 #pragma once
 #include "../PlantAbility.hpp"
 
+static const int max_health[6] = { 300, 400, 400, 500, 500, 500 };
+
 namespace PlantAbility
 {
 	class Mangosteen : public BasePlant
 	{
+	public:
 		void onCreated(MyPlant plant)
 		{
 			plant.ShootOrProductInterval = 150;
+		}
+		void onUpgrade(MyPlant plant)
+		{
+			plant.SetMaxHealth(max_health[plant.Level]);
 		}
 	};
 }
