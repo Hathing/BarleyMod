bc192986c70889d9382e1b9423a33a32b48503c2
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-27 07:47:34 +0800

    fix(Plant): build bug fix

--- pvzdll/MyPlant/implement/Blover.hpp
+++ pvzdll/MyPlant/implement/Blover.hpp
@@ -1,12 +1,11 @@
 #pragma once
 #include "../PlantAbility.hpp"
 
-static const int max_health[6] = { 300, 450, 450, 600, 600, 600 };
-
 namespace PlantAbility
 {
 	class Blover : public BasePlant
 	{
+		inline static const int max_health[6] = { 300, 450, 450, 600, 600, 600 };
 		void onUpgrade(MyPlant plant)
 		{
 			plant.SetMaxHealth(max_health[plant.Level]);
