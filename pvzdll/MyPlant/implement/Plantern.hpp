3c80d7e9bdc29cd5ede3c1a3239eb8c2fa02c69d
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-26 12:09:35 +0800

    feat(Plant): Plantern onAnimate

--- pvzdll/MyPlant/implement/Plantern.hpp
+++ pvzdll/MyPlant/implement/Plantern.hpp
@@ -11,5 +11,20 @@ namespace PlantAbility
 			plant.MaxHp = 4000;
 			plant.RespawnType = SeedType::None;
 		}
+		bool onAnimate(MyPlant plant)
+		{
+			auto model = plant.GetAnimationPart1();
+			if (model.isValid())
+			{
+				int partition = plant.Hp * 6 / plant.MaxHp;
+
+				model.AssignRenderGroupToPrefix(partition < 1 ? -1 : 0, "Plantern_leaf3");
+				model.AssignRenderGroupToPrefix(partition < 2 ? -1 : 0, "Plantern_leaf1");
+				model.AssignRenderGroupToPrefix(partition < 3 ? -1 : 0, "Plantern_leaf4");
+				model.AssignRenderGroupToPrefix(partition < 4 ? -1 : 0, "Plantern_leaf2");
+				model.AssignRenderGroupToPrefix(partition < 5 ? -1 : 0, "Plantern_leaf5");
+			}
+			return false;
+		}
 	};
 }
