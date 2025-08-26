06a1974fd4bc55807365d387defa1adc25ca27e5
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-26 11:47:19 +0800

    feat(Plant): GloomShroom Upgrade

--- pvzdll/MyPlant/implement/GloomShroom.hpp
+++ pvzdll/MyPlant/implement/GloomShroom.hpp
@@ -1,14 +1,20 @@
 #pragma once
 #include "../PlantAbility.hpp"
 
+static const int max_health[6] = { 800, 1000, 1200, 1500, 1500, 1500 };
+
 namespace PlantAbility
 {
 	class GloomShroom : public BasePlant
 	{
 		void onCreated(MyPlant plant)
 		{
-			plant.Hp = 800;
-			plant.MaxHp = 800;
+			plant.Hp = max_health[0];
+			plant.MaxHp = max_health[0];
+		}
+		void onUpgrade(MyPlant plant)
+		{
+			plant.SetMaxHealth(max_health[plant.Level]);
 		}
 	};
 }
