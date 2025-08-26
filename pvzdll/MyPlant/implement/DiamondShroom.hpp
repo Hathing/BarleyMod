992bed6e6fddd0d32fb8416f559095e33c7f4b37
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-26 11:49:02 +0800

    feat(Plant): DiamondShroom Upgrade

--- pvzdll/MyPlant/implement/DiamondShroom.hpp
+++ pvzdll/MyPlant/implement/DiamondShroom.hpp
@@ -1,6 +1,8 @@
 #pragma once
 #include "../PlantAbility.hpp"
 
+static const int max_health[6] = { 300, 400, 400, 500, 500, 500 };
+
 namespace PlantAbility
 {
 	class DiamondShroom : public BasePlant
@@ -13,6 +15,10 @@ namespace PlantAbility
 		void onKill(MyPlant plant, MyZombie zombie)
 		{
 			plant.Heal(25);
+		}
+		void onUpgrade(MyPlant plant)
+		{
+			plant.SetMaxHealth(max_health[plant.Level]);
 		}
 	};
 }
