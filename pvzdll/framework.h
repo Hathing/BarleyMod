84dcd6b78d4e50537b8dd1464599edfea26ff1a4
Lenovo <Lenovo@LAPTOP-UEK0SPL8>
2025-08-26 16:55:39 +0800

    feat(proj): 增加三线子弹的正弦运动

--- pvzdll/framework.h
+++ pvzdll/framework.h
@@ -1,7 +1,15 @@
-#pragma once
+﻿#pragma once
 
 #include <windows.h>
 #include "../pvzclass/pvzclass.h"
 #include "../pvzclass/Events/Events.h"
 
 typedef bool mybool;
+
+
+/// @brief 各种子弹的特殊标记
+typedef unsigned char ProjSpecialFlags;
+/// @brief 三线发射子弹的标记，向边路运动
+constexpr ProjSpecialFlags PSF_THREEPEATER_SLIDE_OUT = 51;
+/// @brief 三线发射子弹的标记，向中路运动
+constexpr ProjSpecialFlags PSF_THREEPEATER_SLIDE_IN = 52;
