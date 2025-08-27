a05ea6d05d32add674c2e052989ef47894a1adfd
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-27 08:57:23 +0800

    feat(Zombie): ZombieSquishPlantEvent

--- pvzdll/MyEvents.hpp
+++ pvzdll/MyEvents.hpp
@@ -293,4 +293,17 @@ namespace PVZEvent
 		CreateAtlasEvent(int address) : BoolDLLEventTemplate() { Init(address); };
 		CreateAtlasEvent() : CreateAtlasEvent("onCreateAtlas") {};
 	};
+
+	/// @brief 僵尸碾压植物事件
+	/// @note 僵王踩踏和砸车暂时不触发此事件。
+	/// @param 触发事件的僵尸，被碾压的行数，被碾压的列数，僵尸攻击类型，被碾压的植物
+	/// @return 是否继续结算碾压。若取消，碾压不会发生。
+	class ZombieSquishPlantEvent : public BoolDLLEventTemplate<0x52E971, 9, 0x52E980, REG_ECX, MEM_ESP_ADD(0x40),
+		MEM_ESP_ADD(0x3C), MEM_ESP_ADD(0x44), REG_EDI>
+	{
+	public:
+		ZombieSquishPlantEvent(const char* str) : BoolDLLEventTemplate() { Init(str); };
+		ZombieSquishPlantEvent(int address) : BoolDLLEventTemplate() { Init(address); };
+		ZombieSquishPlantEvent() : ZombieSquishPlantEvent("onZombieSquishPlant") {};
+	};
 };
--- pvzdll/ZombieEvents.cpp
+++ pvzdll/ZombieEvents.cpp
@@ -1,4 +1,4 @@
-﻿#include "pch.h"
+#include "pch.h"
 #include "MyPlant/PlantAbility.hpp"
 #include "MyZombie/ZombieAbility.hpp"
 #include "MyEvents.hpp"
@@ -74,9 +74,15 @@ void onRandomZombieDropHelm(MyZombie zombie)
 	return;
 }
 
+bool onZombieSquishPlant(MyZombie zombie, int row, int column, int attack_type, MyPlant plant)
+{
+	return true;
+}
+
 void InitZombieEvents()
 {
 	ZombieDropLootEvent((int)onZombieDropLoot);
 	ZombieInitAfterEvent((int)onZombieInitAfter);
 	PVZEvent::RandomZombieDropHelmEvent((int)onRandomZombieDropHelm);
+	PVZEvent::ZombieSquishPlantEvent((int)onZombieSquishPlant);
 }
