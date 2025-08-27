79ec4e3919ec25313d66c200e3e4d5da70abcbcf
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-27 09:12:52 +0800

    feat(Zombie): TallNutZombie Health

--- pvzdll/MyZombie/implement/TallNutZombie.hpp
+++ pvzdll/MyZombie/implement/TallNutZombie.hpp
@@ -11,6 +11,9 @@ namespace ZombieAbility
 			auto model = zombie.GetSpecialHeadAnimation();
 			model.AssignRenderGroupToPrefix(-1, "easter");
 
+			zombie.HelmHealth = 4000;
+			zombie.HelmMaxHealth = 4000;
+
 			zombie.ShieldType = ShieldType::ScreenDoor;
 			zombie.ShieldHealth = 1100;
 			zombie.ShieldMaxHealth = 1100;
