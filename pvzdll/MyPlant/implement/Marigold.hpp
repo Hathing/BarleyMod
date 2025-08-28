a70bb30c9c4b65fffcb7abf18f9d95859bff52e7
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-28 08:36:12 +0800

    feat(Plant): Marigold upgrade

--- pvzdll/MyPlant/implement/Marigold.hpp
+++ pvzdll/MyPlant/implement/Marigold.hpp
@@ -5,9 +5,16 @@ namespace PlantAbility
 {
 	class Marigold : public BasePlant
 	{
+		inline static const int interval[6] = { 3000, 3000, 2500, 2500, 2000, 2000 };
+		inline static const int max_health[6] = { 300, 450, 450, 600, 600, 600 };
 		void onCreated(MyPlant plant)
 		{
-			plant.ShootOrProductInterval = 3000;
+			plant.ShootOrProductInterval = interval[0];
+		}
+		void onUpgrade(MyPlant plant)
+		{
+			plant.ShootOrProductInterval = interval[plant.Level];
+			plant.SetMaxHealth(max_health[plant.Level]);
 		}
 	};
 }
