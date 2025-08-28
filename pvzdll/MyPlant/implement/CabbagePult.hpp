3abb967a027e28da0e1d03072cabdec1f3bcc68f
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-08-28 08:38:23 +0800

    feat(Plant): CabbagePult upgrade

--- pvzdll/MyPlant/implement/CabbagePult.hpp
+++ pvzdll/MyPlant/implement/CabbagePult.hpp
@@ -5,9 +5,14 @@ namespace PlantAbility
 {
 	class CabbagePult : public BasePlant
 	{
+		inline static const int interval[6] = { 500, 480, 480, 460, 460, 460 };
 		void onCreated(MyPlant plant)
 		{
-			plant.ShootOrProductInterval = 500;
+			plant.ShootOrProductInterval = interval[0];
+		}
+		void onUpgrade(MyPlant plant)
+		{
+			plant.ShootOrProductInterval = interval[plant.Level];
 		}
 	};
 }
