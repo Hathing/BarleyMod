0b9f5aa1e470672361f26b73a80881165fe21666
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-09-03 10:14:17 +0800

    feat(Plant): SpringEquinoxVine onDie

--- pvzdll/MyClasses.hpp
+++ pvzdll/MyClasses.hpp
@@ -4,3 +4,35 @@
 #include "MyZombie/MyZombie.hpp"
 
 typedef PVZEvent::PlantDamageZombieEvent::PZDamageInfo<MyPlant, MyZombie> PZDamageEvent;
+
+namespace PVZ
+{
+	template<typename T>
+	T GetByID(int id)
+	{
+		if (id)
+		{
+			T tmp = T(ID_INDEX(id));
+			if (tmp.Id != id)
+				return T(INVALID_BASEADDRESS);
+			else
+				return tmp;
+		}
+		else
+			return T(INVALID_BASEADDRESS);
+	}
+	template<typename T>
+	T GetByID(unsigned int id)
+	{
+		if (id)
+		{
+			T tmp = T(ID_INDEX(id));
+			if (tmp.Id != id)
+				return T(INVALID_BASEADDRESS);
+			else
+				return tmp;
+		}
+		else
+			return T(INVALID_BASEADDRESS);
+	}
+}
