ee61fccc9f55f312d39dac3b748bfb62cc76310d
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-09-04 09:53:37 +0800

    fix(Plant): DiamondShroom upgrade bug fix

--- pvzdll/MyPlant/implement/DiamondShroom.hpp
+++ pvzdll/MyPlant/implement/DiamondShroom.hpp
@@ -4,13 +4,18 @@
 
 namespace PlantAbility
 {
-	inline static const int max_health[6] = { 300, 400, 400, 500, 500, 500 };
 	class DiamondShroom : public NoEasterSkinPlant
 	{
+		inline static const int max_health[6] = { 300, 400, 400, 500, 500, 500 };
 		void onCreated(MyPlant plant)
 		{
 			plant.ImageY -= 20;
 			plant.ShootOrProductInterval = 300;
+			plant.SetMaxHealth(max_health[0]);
+		}
+		void onUpgrade(MyPlant plant)
+		{
+			plant.SetMaxHealth(max_health[plant.Level]);
 		}
 		void onKill(MyPlant plant, MyZombie zombie)
 		{
