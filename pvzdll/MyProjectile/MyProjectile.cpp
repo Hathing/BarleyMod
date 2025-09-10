7dcbf50a98c187ad7612658f464de2f449bb412e
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-09-10 10:16:32 +0800

    feat(Proj): Add Butter

--- pvzdll/MyProjectile/MyProjectile.cpp
+++ pvzdll/MyProjectile/MyProjectile.cpp
@@ -7,7 +7,7 @@ namespace ProjectileAbility
 	{
 		new BaseProjectile(), new BaseProjectile(), new Cabbage(),		  new Melon(), new BaseProjectile(),
 		new BaseProjectile(), new Fireball(), new Star(),			  new BaseProjectile(), new BaseProjectile(),
-		new Kernel(),		  new BaseProjectile(), new BaseProjectile(), new BaseProjectile(), new BaseProjectile(),
+		new Kernel(),		  new BaseProjectile(), new Butter(),		  new BaseProjectile(), new BaseProjectile(),
 		new GoldSpike(),	  new BaseProjectile(), new BaseProjectile(), new Diamond(),		new BaseProjectile(),
 		new BaseProjectile(), new BaseProjectile(), new GoldMelon(),	  new BaseProjectile(),	new BaseProjectile(),
 	};
