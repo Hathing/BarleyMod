ac36140d36e16bfa2ef85f60d8862ac863d893cd
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-09-12 11:35:50 +0800

    feat(Zombie): RandomZombie type pool

--- pvzdll/ZombieEvents.cpp
+++ pvzdll/ZombieEvents.cpp
@@ -66,16 +66,63 @@ void onZombieInitAfter(MyZombie zombie)
 	ZombieAbility::GetAbility(zombie.Type)->onCreated(zombie);
 }
 
+static const ZombieType::ZombieType hypno_pool[] =
+{
+	ZombieType::Zombie,				ZombieType::FlagZombie,			ZombieType::ConeheadZombie, ZombieType::PoleVaultingZombie, ZombieType::BucketheadZombie,
+	ZombieType::NewspaperZombie,	ZombieType::ScreenDoorZombie,	ZombieType::FootballZombie, ZombieType::DancingZombie,		ZombieType::BackupDancer,
+	ZombieType::DuckyTubeZombie,	ZombieType::SnorkedZombie,		ZombieType::Zomboin,		ZombieType::ZombieBobsledTeam,	ZombieType::DolphinRiderZombie,
+	ZombieType::JackintheboxZombie,	ZombieType::BalloonZombie,		ZombieType::DiggerZombie,	ZombieType::PogoZombie,			ZombieType::ZombieYeti,
+									ZombieType::LadderZombie,		ZombieType::CatapultZombie, ZombieType::Gargantuar,			ZombieType::Imp,
+									ZombieType::PeashooterZombie,	ZombieType::WallnutZombie,	ZombieType::JalapenoZombie,		ZombieType::GatlingPeaZombie,
+									ZombieType::TallnutZombie,		ZombieType::Gigagargantuar,
+};
+
 void onRandomZombieDropHelm(MyZombie zombie)
 {
 	if (zombie.Type != ZombieType::ConeheadZombie)
 		return;
 
 	zombie.Remove();
-	int type = Creator::Rand(33);
-	if (type == 25)type = 26;//僵王换豌豆
-	if (type == 20)type = 27;//蹦极换坚果
-	MyZombie child_zombie{ Creator::CreateZombie(static_cast<ZombieType::ZombieType>(type), zombie.Row, 0x0f) };
+
+	int type = 0, elite_type = -1;
+	if (zombie.Hypnotized)
+		type = ::hypno_pool[Creator::Rand(sizeof(hypno_pool) / sizeof(ZombieType::ZombieType))];
+	else
+	{
+		elite_type = Creator::Rand(2);
+		type = Creator::Rand(33);
+		if (type == 25)type = 26;//僵王换豌豆
+		if (type == 20)type = 27;//蹦极换坚果
+
+		switch (type)
+		{
+		case ZombieType::Zombie:
+		case ZombieType::NewspaperZombie:
+		case ZombieType::JackintheboxZombie:
+		case ZombieType::PogoZombie:
+		case ZombieType::CatapultZombie:
+		case ZombieType::SquashZombie:
+		case ZombieType::PeashooterZombie:
+		case ZombieType::WallnutZombie:
+		case ZombieType::TallnutZombie:
+		case ZombieType::Gigagargantuar:
+			elite_type = -1;
+			break;
+		case ZombieType::FootballZombie:
+			if (elite_type > 0)
+				elite_type = 2;
+			break;
+		case ZombieType::FlagZombie:
+		case ZombieType::ScreenDoorZombie:
+			elite_type = Creator::Rand(3);
+			break;
+		}
+
+		elite_type = elite_type > 0 ? elite_type + WAVE_ELITE_MASK : -1;
+	}
+
+	MyBoard board = zombie.GetBoard();
+	MyZombie child_zombie{ board.AddZombieInRow(static_cast<ZombieType::ZombieType>(type), zombie.Row, elite_type) };
 	PVZ::CreateParticleSystem(zombie.X + 40.0f,zombie.Y + 65.0f,zombie.Layer+100,EffectType::IMITATER_TRANSFORMING);
 	child_zombie.X = zombie.X;
 
