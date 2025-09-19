8bfa742ef4ebdd5fe52c6addf1941399458dac99
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-09-19 19:57:44 +0800

    feat(Zombie): Add ScreenDoorZombie

--- pvzdll/MyZombie/MyZombie.cpp
+++ pvzdll/MyZombie/MyZombie.cpp
@@ -6,7 +6,7 @@ namespace ZombieAbility
 	ZombiePTR pt_factory[] =
 	{
 		new BaseZombie(), new BaseZombie(), new BaseZombie(), new PoleVaulter(), new BaseZombie(),
-		new BaseZombie(), new BaseZombie(), new BaseZombie(), new DancingZombie(), new BaseZombie(),
+		new BaseZombie(), new ScreenDoorZombie(), new BaseZombie(), new DancingZombie(), new BaseZombie(),
 		new BaseZombie(), new SnorkedZombie(), new Zomboni(),    new BaseZombie(), new BaseZombie(),
 		new ClownZombie(), new BaseZombie(), new DiggerZombie(), new PogoZombie(), new BaseZombie(),
 		new BungeeZombie(), new BaseZombie(), new CatapultZombie(), new Gargantaur(), new BaseZombie(),
