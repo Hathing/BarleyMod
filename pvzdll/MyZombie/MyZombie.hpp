50646b422a9b053552562ca87333b033dc92f042
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-09-19 19:22:07 +0800

    feat(Zombie): member update

--- pvzdll/MyZombie/MyZombie.hpp
+++ pvzdll/MyZombie/MyZombie.hpp
@@ -25,6 +25,10 @@ public:
 	T_PROPERTY(byte, GhostFlameMark, __get_GhFM, __set_GhFM, 0x106);
 	/// @brief 被魅惑菇召唤时，魅惑菇的等级
 	T_PROPERTY(byte, SourceLevel, __get_SoL, __set_SoL, 0x107);
+	/// @brief 豌豆僵尸变种类型
+	INT_PROPERTY(PeaHeadType, __get_PeHT, __set_PeHT, 0x138);
+	/// @brief 大蒜状态
+	INT_PROPERTY(GarlicStatus, __get_GaS, __set_GaS, 0x13C);
 	/// @brief 僵尸反向标记，1=反向回头，2=反向出屏幕
 	T_PROPERTY(byte, IsWalkingBackwards, __get_IsWalkingBackwards, __set_IsWalkingBackwards, 0x13C);
 	/// @brief 僵尸寒意层数，上限30层，每层减速2%
