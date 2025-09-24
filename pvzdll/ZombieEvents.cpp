a18b56c426599515da10c3c6e2df9542119ef093
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-09-24 09:28:08 +0800

    feat(RandZombie): RandZombie gen move out

--- pvzdll/ZombieEvents.cpp
+++ pvzdll/ZombieEvents.cpp
@@ -69,85 +69,6 @@ void onZombieInitAfter(MyZombie zombie)
 	ZombieAbility::GetAbility(zombie.Type)->onCreated(zombie);
 }
 
-static const ZombieType::ZombieType hypno_pool[] =
-{
-	ZombieType::Zombie,				ZombieType::FlagZombie,			ZombieType::ConeheadZombie,									ZombieType::BucketheadZombie,
-	ZombieType::NewspaperZombie,	ZombieType::ScreenDoorZombie,	ZombieType::FootballZombie, ZombieType::DancingZombie,		ZombieType::BackupDancer,
-	ZombieType::DuckyTubeZombie,									ZombieType::Zomboin,		ZombieType::ZombieBobsledTeam,
-	ZombieType::JackintheboxZombie,	ZombieType::BalloonZombie,		ZombieType::DiggerZombie,									ZombieType::ZombieYeti,
-									ZombieType::LadderZombie,									ZombieType::Gargantuar,			ZombieType::Imp,
-									ZombieType::PeashooterZombie,	ZombieType::WallnutZombie,	ZombieType::JalapenoZombie,		ZombieType::GatlingPeaZombie,
-									ZombieType::TallnutZombie,		ZombieType::Gigagargantuar,
-};
-
-void onRandomZombieDropHelm(MyZombie zombie)
-{
-	if (zombie.Type != ZombieType::ConeheadZombie)
-		return;
-
-	zombie.Remove();
-
-	int type = 0, elite_type = -1;
-	if (zombie.Hypnotized)
-		type = ::hypno_pool[Creator::Rand(sizeof(hypno_pool) / sizeof(ZombieType::ZombieType))];
-	else
-	{
-		elite_type = Creator::Rand(2);
-		type = Creator::Rand(33);
-		if (type == 25)type = 26;//僵王换豌豆
-		if (type == 20)type = 27;//蹦极换坚果
-
-		switch (type)
-		{
-		case ZombieType::Zombie:
-		case ZombieType::NewspaperZombie:
-		case ZombieType::JackintheboxZombie:
-		case ZombieType::PogoZombie:
-		case ZombieType::CatapultZombie:
-		case ZombieType::SquashZombie:
-		case ZombieType::PeashooterZombie:
-		case ZombieType::WallnutZombie:
-		case ZombieType::TallnutZombie:
-		case ZombieType::Gigagargantuar:
-			elite_type = -1;
-			break;
-		case ZombieType::FootballZombie:
-			if (elite_type > 0)
-				elite_type = 2;
-			break;
-		case ZombieType::FlagZombie:
-		case ZombieType::ScreenDoorZombie:
-			elite_type = Creator::Rand(3);
-			break;
-		}
-
-		elite_type = elite_type > 0 ? elite_type + WAVE_ELITE_MASK : -1;
-	}
-
-	MyBoard board = zombie.GetBoard();
-	MyZombie child_zombie{ board.AddZombieInRow(static_cast<ZombieType::ZombieType>(type), zombie.Row, elite_type) };
-	PVZ::CreateParticleSystem(zombie.X + 40.0f,zombie.Y + 65.0f,zombie.Layer+100,EffectType::IMITATER_TRANSFORMING);
-	child_zombie.X = zombie.X;
-
-	if (true)
-	{
-		int HpPoint = 1 + Creator::Rand(5);
-		if (CLOWN_ZOMBIE_POP_FLAG)
-			HpPoint = 5;
-		child_zombie.HpPoint = HpPoint;
-		float health_ratio = (HpPoint) / 5.0f;
-
-		child_zombie.BodyHealth *= health_ratio;
-		child_zombie.BodyMaxHealth *= health_ratio;
-		child_zombie.HelmHealth *= health_ratio;
-		child_zombie.HelmMaxHealth *= health_ratio;
-		child_zombie.ShieldHealth *= health_ratio;
-		child_zombie.ShieldMaxHealth *= health_ratio;
-		///651185跳到了651373和6512d5,不知道是干啥的一段代码，没搬
-	}
-	return;
-}
-
 int onPlantTakeDamage(MyPlant plant, PVZ::BaseClass source, GameObjectType::GameObjectType source_type, int damage)
 {
 	return damage;
@@ -576,7 +497,6 @@ void InitZombieEvents()
 	PlantTakeDamageEvent((int)onPlantTakeDamage);
 	ZombieDropLootEvent((int)onZombieDropLoot);
 	ZombieInitAfterEvent((int)onZombieInitAfter);
-	PVZEvent::ZombieDropHelmByDamageEvent((int)onRandomZombieDropHelm);
 	PVZEvent::ZombieSquishPlantEvent((int)onZombieSquishPlant);
 	PVZEvent::LoadPlainZombieReanimBeforeEvent((int)onLoadPlainZombieReanimBefore);
 	PVZEvent::ZombieUpdateWalkingSpeedEvent((int)onZombieUpdateWalkingSpeed);
