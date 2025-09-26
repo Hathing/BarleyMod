fd74dbcbbbe38607f35d5c9ed6e625315b242c66
Lenovo <Lenovo@LAPTOP-UEK0SPL8>
2025-09-26 23:34:37 +0800

    feat(plant): 搬运西瓜投手随等级提升的技能

--- pvzdll/MyPlant/implement/MelonPult.hpp
+++ pvzdll/MyPlant/implement/MelonPult.hpp
@@ -6,15 +6,22 @@ namespace PlantAbility
 {
 	class MelonPult : public NoEasterSkinPlant
 	{
+		inline static const int bounce_count[6] = { 0,0,1,1,2,2 };
+		inline static const int size_stack[6] = { 2,4,4,6,6,6 };
 		void onCreated(MyPlant plant)
 		{
 			plant.ShootOrProductInterval = 300;
 		}
 		bool onAddProjectile(MyPlant plant, MyProjectile proj, MyZombie zombie, int PlantWeapon)
 		{
-			while (Creator::Rand(100) < 90)
+			proj.BounceCount = bounce_count[plant.Level];
+			proj.SpecialStack += size_stack[plant.Level];
+			if (plant.Level >= 5)
 			{
-				proj.SpecialStack += 1;
+				while (Creator::Rand(100) < 90)
+				{
+					proj.SpecialStack += 1;
+				}
 			}
 			return true;
 		}
