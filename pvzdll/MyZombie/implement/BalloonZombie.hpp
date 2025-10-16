142aaaec98953635174832572269a2736c3b1b29
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-10-16 09:30:32 +0800

    feat(Zombie): GetReanimType Part 1

--- pvzdll/MyZombie/implement/BalloonZombie.hpp
+++ pvzdll/MyZombie/implement/BalloonZombie.hpp
@@ -16,5 +16,9 @@ namespace ZombieAbility
 				zombie.FlyingMaxHealth = 800;
 			}
 		}
+		AnimationType::AnimationType GetReanimType(MyZombie zombie, AnimationType::AnimationType type)
+		{
+			return zombie.FromWave == WAVE_ELITE1 ? (AnimationType::AnimationType)0x95 : type;
+		}
 	};
 }
