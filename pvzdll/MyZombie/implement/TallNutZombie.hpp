6cf2469e022b98f7970f943390eac4cee5af519a
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-10-20 17:41:45 +0800

    feat(TallnutZombie): helm texture

--- pvzdll/MyZombie/implement/TallNutZombie.hpp
+++ pvzdll/MyZombie/implement/TallNutZombie.hpp
@@ -19,5 +19,20 @@ namespace ZombieAbility
 			zombie.ShieldMaxHealth = 1100;
 			zombie.AttachShield();
 		}
+		bool OverrideHelmDamageTexture(MyZombie zombie, PVZ::Animation reanim, int partition)
+		{
+			switch (partition)
+			{
+			case 1:
+				reanim.SetImageOverride("common_idle", *((PVZ::Image*)0x6A7200));
+				return false;
+			case 2:
+				reanim.SetImageOverride("common_idle", *((PVZ::Image*)0x6A77D8));
+				return false;
+			default:
+				return false;
+			}
+			return false;
+		}
 	};
 }
