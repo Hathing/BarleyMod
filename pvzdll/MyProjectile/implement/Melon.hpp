3efd8b82e63b02187989447d440c5588a9851276
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-10-24 10:46:05 +0800

    feat(Melon): proj image

--- pvzdll/MyProjectile/implement/Melon.hpp
+++ pvzdll/MyProjectile/implement/Melon.hpp
@@ -1,4 +1,4 @@
-﻿#pragma once
+#pragma once
 #include "../ProjectileAbility.hpp"
 #include <cmath>
 
@@ -14,6 +14,18 @@ namespace ProjectileAbility
 			else if (damage_type == PVZEvent::ProjDmgType::DAMAGE_SPLASH_SECONDARY)
 				return ori_dmg + proj.SpecialStack * 10;//原CT数值显然不合理，不过暂时不管
 			return -1;
+		}
+		int GetImage(MyProjectile proj)
+		{
+			switch (proj.SourceType)
+			{
+			case SeedType::DarkShroom:
+				return PVZ::Memory::ReadMemory<int>(0x6FF154);
+			case SeedType::Mangosteen:
+				return PVZ::Memory::ReadMemory<int>(0x6FF158);
+			default:
+				return -1;
+			}
 		}
 		float GetImageSize(MyProjectile proj, float original_val)
 		{
