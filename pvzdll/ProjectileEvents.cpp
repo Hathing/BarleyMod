34edbcc765764b5304e26da7eccfa3cab158fa5a
Lenovo <Lenovo@LAPTOP-UEK0SPL8>
2025-10-25 01:28:27 +0800

    feat(proj): 加农炮撞地距离与特效坐标修改

--- pvzdll/ProjectileEvents.cpp
+++ pvzdll/ProjectileEvents.cpp
@@ -356,4 +356,10 @@ void InitProjectileEvents()
 	PVZ::Memory::WriteMemory<byte>(0x46D2A1, 0);
 	//黄油定身时间缩短为2s
 	PVZ::Memory::WriteMemory<int>(0x532741, 200);
+	//加农炮碰地距离变低
+	static const float cobcannon_collide_ground_height = 40.0f;
+	PVZ::Memory::WriteMemory<int>(0x46D672, (unsigned int)((void*)&cobcannon_collide_ground_height));
+	//加农炮碰地特效贴图坐标修改
+	static const double cobcannon_collide_effect_yoffset = 120.0f;
+	PVZ::Memory::WriteMemory<int>(0x46E14F, (unsigned int)((void*)&cobcannon_collide_effect_yoffset));
 }
