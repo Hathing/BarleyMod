768c15a090a00660b59049e272377f668269cb53
Lenovo <Lenovo@LAPTOP-UEK0SPL8>
2025-10-24 15:49:36 +0800

    fix(plant): 胆小菇击杀层数初始化

--- pvzdll/MyPlant/implement/ScaredyShroom.hpp
+++ pvzdll/MyPlant/implement/ScaredyShroom.hpp
@@ -6,6 +6,10 @@ namespace PlantAbility
 {
 	class ScaredyShroom : public NoEasterSkinPlant
 	{
+		void onCreated(MyPlant plant)
+		{
+			plant.ScaredyShroomKillCount = 0;
+		}
 		void onKill(MyPlant plant, MyZombie zombie)
 		{
 			plant.ScaredyShroomKillCount++;
