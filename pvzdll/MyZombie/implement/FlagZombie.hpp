1c22b97cb1ce5ce3af4067a4210043dcae36c3b2
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-10-24 11:17:59 +0800

    feat(FlagZombie): normal ability

--- pvzdll/MyZombie/implement/FlagZombie.hpp
+++ pvzdll/MyZombie/implement/FlagZombie.hpp
@@ -6,8 +6,12 @@ namespace ZombieAbility
 	class FlagZombie : public BaseZombie
 	{
 	public:
+		inline static constexpr ZombieType::ZombieType summon_pool[4] = {ZombieType::Undying, ZombieType::BackupDancer,
+			ZombieType::DuckyTubeZombie, ZombieType::ZombieBobsledTeam};
 		void onCreated(MyZombie zombie)
 		{
+			zombie.SummonCounter = 100;
+
 			auto model = zombie.GetAnimation();
 			switch (zombie.FromWave)
 			{
@@ -25,6 +29,17 @@ namespace ZombieAbility
 		AnimationType::AnimationType GetReanimType(MyZombie zombie, AnimationType::AnimationType type)
 		{
 			return zombie.FromWave == WAVE_ELITE1 ? (AnimationType::AnimationType)0x0A1 : type;
+		}
+		bool TickAbility(MyZombie zombie)
+		{
+			zombie.SummonCounter--;
+			if (!zombie.SummonCounter)
+			{
+				MyBoard board = zombie.GetBoard();
+				board.AddZombieInRow(summon_pool[Creator::Rand(4)], zombie.Row, -1);
+			}
+
+			return false;
 		}
 	};
 }
