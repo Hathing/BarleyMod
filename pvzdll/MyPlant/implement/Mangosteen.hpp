30b77e1e2e4460d10a5360c972f0d9520d5e5da2
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-10-25 15:47:15 +0800

    feat(Mangosteen): multishot

--- pvzdll/MyPlant/implement/Mangosteen.hpp
+++ pvzdll/MyPlant/implement/Mangosteen.hpp
@@ -9,7 +9,7 @@ namespace PlantAbility
 		inline static const int max_health[6] = { 300, 400, 400, 500, 500, 500 };
 		void onCreated(MyPlant plant)
 		{
-			plant.ShootOrProductInterval = 150;
+			plant.ShootOrProductInterval = 130;
 		}
 		void onUpgrade(MyPlant plant)
 		{
@@ -18,6 +18,13 @@ namespace PlantAbility
 		int GetEasterProbabilityPartition()
 		{
 			return 1;
+		}
+		bool onUpdateShooter(MyPlant plant)
+		{
+			if (plant.ShootOrProductCountdown == 15 || plant.ShootOrProductCountdown == 30
+					|| plant.ShootOrProductCountdown == 45 || plant.ShootOrProductCountdown == 60)
+				plant.FindTargetAndFire(plant.Row, 0);
+			return true;
 		}
 	};
 }
