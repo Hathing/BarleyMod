051fd8c1b4f54ad09c0ba7997b72143dc13d0e8b
W1thoutD0ubt <106100307+W1thoutD0ubt@users.noreply.github.com>
2025-11-06 16:31:02 +0800

    feat(Umbrella): imp

--- pvzdll/MyPlant/implement/UmbrellaLeaf.hpp
+++ pvzdll/MyPlant/implement/UmbrellaLeaf.hpp
@@ -19,15 +19,21 @@ namespace PlantAbility
 		{
 			auto zombies = plant.GetBoard().GetAllZombies<MyZombie>();
 			for (auto zombie : zombies)
-				if (zombie.Row == plant.Row && (zombie.State == ZombieState::POLE_VALUTING_JUMPPING
+				if (zombie.Row == plant.Row && !zombie.Blowaway && (zombie.State == ZombieState::POLE_VALUTING_JUMPPING
 						|| zombie.State == ZombieState::POGO_JUMP_ACROSS
 						|| zombie.State == ZombieState::DOPHIN_JUMP
 						|| zombie.State == ZombieState::SNORKEL_JUMP_IN_THE_POOL
-						|| zombie.State == ZombieState::BALLOON_FALLING))
+						|| zombie.State == ZombieState::BALLOON_FALLING
+						|| zombie.State == ZombieState::IMP_FLYING))
 					if (plant.ImageX - zombie.X < 80 && plant.ImageX - zombie.X > 160)
 					{
 						zombie.Blowaway = 1;
 						zombie.Layer = 0x61A80 + zombie.Row * 10000;
+						if (zombie.State == ZombieState::IMP_FLYING)
+						{
+							PVZ::Memory::WriteMemoryUnsafe<float>(zombie.GetBaseAddress() + 0x120, 5.0f);
+							zombie.Speed *= -2;
+						}
 
 						Creator::CreateUpperSound(UpperSoundType::FutureOn);
 						plant.CreateEffect();
