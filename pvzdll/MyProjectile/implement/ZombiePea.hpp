3e1fbae5498100112a4a2924c5e3b4fd3bcc8f89
Lenovo <Lenovo@LAPTOP-UEK0SPL8>
2026-07-13 23:45:40 +0800

    fix: 改UTF-8

--- pvzdll/MyProjectile/implement/ZombiePea.hpp
+++ pvzdll/MyProjectile/implement/ZombiePea.hpp
@@ -1,4 +1,4 @@
-#pragma once
+ï»¿#pragma once
 #include "../ProjectileAbility.hpp"
 
 namespace ProjectileAbility
@@ -9,7 +9,7 @@ namespace ProjectileAbility
 		int GetImage(MyProjectile proj)
 		{
 			if (proj.SpecialFlags == 1)
-				//??Ã»¿´¶®
+				//??æ²¡çœ‹æ‡‚
 				return PVZ::Memory::ReadMemory<int>(0x6a7408);
 			return -1;
 		}
